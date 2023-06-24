270b9bc7edacf2cf046e0e2190af3bfde1edf339
hyzic <isaac.oghenevwapo@gmail.com>
2023-06-24 13:08:35 -0400

    Added Controller to app and center align token generated

--- lib/screens/generate_otp.dart
+++ lib/screens/generate_otp.dart
@@ -29,7 +29,7 @@ class _GenerateTokenWidgetState extends State<GenerateTokenWidget> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String token = '';
-
+  final tokenController = TextEditingController();
   String generateSixDigitsToken() {
     String generatedToken = Random().nextInt(999999).toString().padLeft(6, '0');
     token = generatedToken;
@@ -58,17 +58,21 @@ class _GenerateTokenWidgetState extends State<GenerateTokenWidget> {
               //child: Center(
               child: TextFormField(
                 textAlign: TextAlign.center,
-                decoration: InputDecoration(
+                style: const TextStyle(fontSize: 24),
+                controller: tokenController,
+                decoration: const InputDecoration(
+                  //fillColor: Colors.amber,
+                  //filled: true,
                   disabledBorder: InputBorder.none,
                   border: InputBorder.none,
                   isCollapsed: true,
                   contentPadding: EdgeInsets.zero,
                   alignLabelWithHint: true,
-                  labelText: token,
-                  labelStyle: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 25.0,
-                  ),
+                  //labelText: token,
+                  // labelStyle: const TextStyle(
+                  //   color: Colors.black,
+                  //   fontSize: 25.0,
+                  // ),
                 ),
               ),
               //),
@@ -86,7 +90,8 @@ class _GenerateTokenWidgetState extends State<GenerateTokenWidget> {
               onPressed: () {
                 setState(() {
                   String tokenGenerated = generateSixDigitsToken();
-                  token = tokenGenerated;
+                  tokenController.text = tokenGenerated;
+                  //token = tokenGenerated;
                 });
               },
               child: const Text('Generate OTP',
